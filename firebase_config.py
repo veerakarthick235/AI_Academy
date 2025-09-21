79090fa2b5eae20a4de228ac62c85d59781c3ea5
KARTHICKKUMAR S <veerakarthick235@gmail.com>
2025-09-21 18:14:12 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,20 @@
 import firebase_admin
-from firebase_admin import credentials, firestore, auth
+from firebase_admin import credentials, firestore
+import os
+import json
 
-# Load the service account key
-cred = credentials.Certificate('serviceAccountKey.json')
+# Check if the environment variable exists
+if 'GOOGLE_CREDENTIALS_JSON' in os.environ:
+    # On Render: Load credentials from the environment variable
+    creds_json_str = os.environ.get('GOOGLE_CREDENTIALS_JSON')
+    creds_dict = json.loads(creds_json_str)
+    cred = credentials.Certificate(creds_dict)
+else:
+    # Locally: Load credentials from the JSON file
+    cred = credentials.Certificate('serviceAccountKey.json')
 
-# Initialize the app if it's not already initialized
+# Initialize the app if not already done
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
-# Get a reference to the Firestore database and Auth service
 db = firestore.client()
-firebase_auth = auth
